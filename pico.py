66e33259cc128a01807cea91c125b123577446ef
Physics722 <102660754+Physics722@users.noreply.github.com>
2023-10-18 14:57:06 +0800

    Update pico.py

--- pico.py
+++ pico.py
@@ -2,3 +2,7 @@ import machine
 import serial
 
 ser = serial.Serial('COMPORT', baudrate=31250)  # Adjust the baudrate as needed (MIDI standard is 31250).
+
+def send_midi_message(status, data1, data2):
+    midi_message = bytes([status, data1, data2])
+    ser.write(midi_message)
